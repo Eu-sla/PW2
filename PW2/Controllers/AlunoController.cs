3ce84af34e53508ab7b5ca5c7cb768e2d7da9f70
230216 <230216@LTI1305>
2025-02-19 15:20:13 -0300

    Adicionar arquivos de projeto.

---
+++ PW2/Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PW2.Models;
+
+namespace PW2.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            return View(Aluno.GerarLista());
+        }
+    }
+}
---
+++ PW2/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PW2.Models
+{
+    public class Aluno
+    {
+
+        public string Nome { get; set; }
+        public string RA { get; set; }
+
+        public static List<Aluno> GerarLista()
+        {
+            var lista = new List<Aluno>();
+            lista.Add(new Aluno { Nome = "Barbie", RA = "234567" });
+            lista.Add(new Aluno { Nome = "sceds", RA = "325432" });
+            lista.Add(new Aluno { Nome = "alfred", RA = "182732" });
+
+            return lista;
+        }
+    }
+}
