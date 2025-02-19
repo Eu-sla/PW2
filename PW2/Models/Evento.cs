6dbf63e54e1366412c2664820cfb5e17e36ce78a
230216 <230216@LTI1305>
2025-02-19 16:27:32 -0300

    ok

---
+++ PW2/Models/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PW2.Models
+{
+    public class Evento
+    {
+        public string Local { get; set; }
+        public DateTime Data { get; set; }
+
+        public static List<Evento> GerarLista()
+        {
+            var lista = new List<Evento>();
+            lista.Add(new Evento() { Local = "Prefeitura", Data = 12 / 02 / 2026 - 16:00 }
+        }
+    }
+}
